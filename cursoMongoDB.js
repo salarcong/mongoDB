b45189914825630a15995879c1d534572d407a91
salarcong <106347308+salarcong@users.noreply.github.com>
2023-12-26 17:10:52 -0600

    main

--- cursoMongoDB.js
+++ cursoMongoDB.js
@@ -1,5 +1,47 @@
-db.users.insertMany(
-   [
-    {}
-   ] 
+db.users.updateOne(
+   {
+      name: 'Esteban'
+   },
+   {
+      $set: {
+         scores: [8,8,9,8,7]
+      }
+   }
 )
+
+//cuando conocemos el indice
+db.users.updateMany(
+   {
+      scores: {$exists: true}
+   },
+   {
+      $set: {
+         'scores.0': 5
+      }
+   }
+)
+
+db.users.find(
+   {
+      scores: {
+         $exists: true
+      }
+   }
+)
+
+
+//cuando no conocemos el indice
+db.users.updateMany(
+   {
+      scores: {$exists: true},
+      scores: 6
+   },
+   {
+      $set: {
+         'scores.$': 7
+      }
+   }
+)
+
+
+
